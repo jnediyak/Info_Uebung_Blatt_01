83ca877f0a1daaf83e5822c8f798839737a264a0
jnediyak <jnediyak@smail.uni-koeln.de>
2025-10-22 18:12:21 +0200

    Hausaufgabe 1: Erste Abgabe

---
+++ uebung/Julian.java
@@ -0,0 +1,10 @@
+package uebung;
+
+public class Julian
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World, die Klasse heißt Julian!");
+		System.out.println("Frage: Welche files sollen alles in Github auftreten?");
+	}
+}
